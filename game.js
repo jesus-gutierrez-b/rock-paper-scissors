ca010a9add0019ef067bdb315bfd037810ac8e0a
Jesus Gutierrez <jesus.gutierrez.b@outlook.com>
2020-06-30 18:10:59 -0700

    Added comments

--- game.js
+++ game.js
@@ -1,19 +1,27 @@
 // Code for the rock, paper, scissors game with GUI
 
+// Variables to keep track of the scores
 let playerScore = 0;
 let computerScore = 0;
 
+// Variables to access html elements and change them with javascript
+
+// Scores
 const pScore = document.getElementById("playerScore");
 const cScore = document.getElementById("computerScore");
 
+// Options to choose for players
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 
+// Player selection, computer selection and round outcome
 const playerSel = document.getElementById("playerSelection");
 const computerSel = document.getElementById("computerSelection"); 
 const roundOutcome = document.getElementById("outcome");
 
+// Events when a player chooses 1 out of the 3 options, note that computer's play
+// is chosen when computerPlay() is called inside playRound function
 rock.addEventListener("click", () => {
     playerSel.src = "images/rock.jpeg";
     playRound("rock", computerPlay());
@@ -31,6 +39,7 @@ scissors.addEventListener("click", () => {
 
 updateScore();
 
+// Restarts score and webpage messages
 function restart(){
     playerScore = 0;
     computerScore = 0;
@@ -40,11 +49,13 @@ function restart(){
     roundOutcome.textContent = "";    
 }
 
+// Updates both scores
 function updateScore(){
     pScore.textContent = `Player: ${playerScore}`;
     cScore.textContent = `Computer: ${computerScore}`;
 }
 
+// Chooses computer play randomly
 function computerPlay(){
     let play = Math.floor(Math.random() *3);
 
@@ -59,12 +70,14 @@ function computerPlay(){
     }
 }// end computerPlay function
 
-function playRound(playerSelection = "rock", computerSelection = "rock"){
+// Evaluates the result between the player and computer selection
+function playRound(playerSelection = "rock", computerSelection = "scissors"){
+    // both player and computer selection are transform to lower case letters
     playerSelection = playerSelection.toLocaleLowerCase();
     computerSelection = computerSelection.toLocaleLowerCase();
-
+    // Computer selection image according to the chosen play
     computerSel.src = `images/${computerSelection}.jpeg`;
-    
+    // Each scenario when the player chooses rock
     if(playerSelection === "rock"){
         if(computerSelection === "scissors"){
             playerScore++;
@@ -77,6 +90,7 @@ function playRound(playerSelection = "rock", computerSelection = "rock"){
         }else{
             roundOutcome.textContent = "Something went terribly wrong.";
         }
+        // Each scenario when the player chooses paper
     }else if(playerSelection === "paper"){
         if(computerSelection === "scissors"){
             computerScore++;
@@ -89,6 +103,7 @@ function playRound(playerSelection = "rock", computerSelection = "rock"){
         }else{
             roundOutcome.textContent = "Something went terribly wrong.";
         }
+        // Each scenario when the player chooses scissors
     }else if(playerSelection === "scissors"){
         if(computerSelection === "scissors"){
             roundOutcome.textContent = "It's a tie! Both chose 'Scissors'. No winner!";
@@ -107,6 +122,7 @@ function playRound(playerSelection = "rock", computerSelection = "rock"){
 
     updateScore();
 
+    // The game finishes when either the player or computer reaches 5 wins
     if(playerScore >= 5){
         alert(`Final Score
 Player: ${playerScore}
